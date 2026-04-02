ed55dbbef9251df314336a330f42b44284528a61
gabrial <gabrial@gdmellccom.com>
2026-04-01 20:59:42 -0400

    refactor middleware to return early for root path

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,9 @@ import type { NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    return;
+  }
   return updateSession(request);
 }
 
