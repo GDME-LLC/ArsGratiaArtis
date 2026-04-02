d5f880e3e7d1459292ba82304fee26b2c80885e4
gabrial <gabrial@gdmellccom.com>
2026-04-01 21:16:55 -0400

    remove early return for root path in middleware function

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,6 @@ import type { NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/") {
-    return;
-  }
   return updateSession(request);
 }
 
